8ed542f5c3cc45c1ab70d63df2622520b669ee88
WirbelWind03 <hidden>
2025-05-16 06:28:26 +0200

    Added step by step

---
+++ cynaspe/src/utils/KruskalMazeGenerator.java
@@ -0,0 +1,44 @@
+package utils;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import model.EdgeModel;
+import model.MazeModel;
+import model.TileModel;
+
+public class KruskalMazeGenerator {
+    private int currentIndex = 0;
+    private List<EdgeModel> edges;
+    private DisjointSet disjointSet;
+
+    public  KruskalMazeGenerator(MazeModel maze){
+        disjointSet = new DisjointSet();
+
+        edges = maze.getEdges();
+        Collections.shuffle(edges);
+
+        for (TileModel[] row : maze.tiles){
+            for (TileModel tile: row){
+                disjointSet.makeSet(tile);
+            }
+        }
+    }
+
+    public boolean step(){
+        if (currentIndex >= edges.size()) return false;
+
+        EdgeModel edge = edges.get(currentIndex++);
+        if (!disjointSet.connected(edge.tile1, edge.tile2)){
+            disjointSet.union(edge.tile1, edge.tile2);
+            edge.tile1.removeWall(edge.tile2);
+        }
+
+        return true;
+    }
+
+    public boolean isComplete() {
+        return currentIndex >= edges.size();
+    }
+}
