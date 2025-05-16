4ad4db793cc7b2ce30d329eeda2f75039ec4dc6a
WirbelWind03 <hidden>
2025-05-16 04:34:10 +0200

    Class that represent the wall

---
+++ cynaspe/src/model/EdgeModel.java
@@ -0,0 +1,10 @@
+package model;
+
+public class EdgeModel {
+    public TileModel tile1, tile2;
+
+    public EdgeModel(TileModel tile1, TileModel tile2){
+        this.tile1 = tile1;
+        this.tile2 = tile2;
+    }
+}
