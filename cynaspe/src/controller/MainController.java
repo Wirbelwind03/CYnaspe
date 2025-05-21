9adefa76eac41112aa52258af664edd2faf8a2ea
WirbelWind03 <hidden>
2025-05-21 05:00:01 +0200

    Removed useless imports

--- cynaspe/src/controller/MainController.java
+++ cynaspe/src/controller/MainController.java
@@ -1,28 +1,17 @@
 package controller;
 
 import enums.DialogResult;
-import enums.GenerationMode;
-import enums.TileStatus;
-import enums.WallDirection;
-import javafx.animation.AnimationTimer;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import model.MazeModel;
-import model.TileModel;
-import utils.KruskalMazeGenerator;
 
 public class MainController {
-    private MazeConfigurationController mazeConfigController;
     private MazeController mazeController;
 
     @FXML
@@ -39,7 +28,7 @@ public class MainController {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/MazeConfigurationView.fxml"));
         Parent root = loader.load();
         // Get the controller of the MazeConfigurationView
-        mazeConfigController = loader.getController();
+        MazeConfigurationController mazeConfigController = loader.getController();
 
         // Show the window for the configuration of the maze
         Stage configStage = new Stage();
