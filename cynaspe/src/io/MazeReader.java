80d1cf3f70a72075a57dcd4fa941076fed144673
Yun000 <126633249+Yun000@users.noreply.github.com>
2025-05-22 14:32:05 +0200

    Add files via upload

---
+++ cynaspe/src/io/MazeReader.java
@@ -0,0 +1,44 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import enums.WallDirection;
+import model.MazeModel;
+import model.TileModel;
+
+public class MazeReader {
+    public static MazeModel read(File file){
+        Path path = file.toPath();
+        try {
+
+            List<String> lines = Files.readAllLines(path);
+            String firstLine = lines.get(0);
+            String[] mazeSizeParts = firstLine.split(",");
+            int numRows = Integer.parseInt(mazeSizeParts[0]);
+            int numCols = Integer.parseInt(mazeSizeParts[1]);
+
+            TileModel[][] tiles = new TileModel[numRows][numCols];
+            for (int i = 1; i < lines.size(); i++) {
+                String line = lines.get(i);
+                String[] parts = line.split(",");
+                int row = Integer.parseInt(parts[0]);
+                int col = Integer.parseInt(parts[1]);
+                String wallBits = parts[2];
+                Map<WallDirection, Boolean> walls = TileModel.getWalls(wallBits);
+                tiles[row][col] = new TileModel(row, col, walls);
+            }
+
+            return new MazeModel(tiles);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }   
+}
