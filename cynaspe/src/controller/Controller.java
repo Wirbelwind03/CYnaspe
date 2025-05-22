f68f26bc64bcf47ba3f3b8965dfca0d4873b5929
WirbelWind03 <hidden>
2025-05-23 00:08:02 +0200

    Added comments

--- cynaspe/src/controller/Controller.java
+++ cynaspe/src/controller/Controller.java
@@ -5,6 +5,12 @@ import javafx.stage.Stage;
 public abstract class Controller {
     public Stage primaryStage;
 
+    /**
+     * Set the primary stage of the controller
+     * Called by the application when initializing the controller
+     * @param stage
+     * The primary stage of the application
+     */
     public void setPrimaryStage(Stage stage) {
         primaryStage = stage;
     }
