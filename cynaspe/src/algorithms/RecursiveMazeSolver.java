399affbdee389f501e5cb1ab26078a415f932fa4
Yassir95 <134860762+Yassir95@users.noreply.github.com>
2025-05-23 01:12:44 +0200

    Update RecursiveMazeSolver.java

--- cynaspe/src/algorithms/RecursiveMazeSolver.java
+++ cynaspe/src/algorithms/RecursiveMazeSolver.java
@@ -1,41 +1,72 @@
-package algorithms;
+import java.util.Stack;
 
 import controller.MazeController;
 import enums.TileStatus;
-import model.MazeModel;
 import model.TileModel;
 
-public class RecursiveMazeSolver {
+public class RecursiveMazeSolver extends Solver implements ISolverAlgorithm {
 
-    private MazeController mazeController;
+private Stack<TileModel> stack = new Stack<>();
 
-    public RecursiveMazeSolver(MazeController mazeController) {
-        this.mazeController = mazeController;
-    }
+public RecursiveMazeSolver(MazeController mazeController) {
+this.mazeController = mazeController;
 
-    public boolean solve() {
-        TileModel start = mazeController.getStartTile();
-        TileModel end = mazeController.getEndTile();
-        return explore(start, end);
-    }
+TileModel start = mazeController.getStartTile();
+stack.push(start);
+visited.add(start);
+}
 
-    private boolean explore(TileModel current, TileModel end) {
-        if (current == null || current.isVisited) return false;
+@Override
+public boolean step() {
+if (isFinished && pathStep != null) {
+pathStep.status = TileStatus.PATH;
+pathCount++;
+pathStep = parentMap.get(pathStep);
+return pathStep == null;
+}
 
-        current.isVisited = true;
+if (!isFinished) {
+if (!stack.isEmpty()) {
+TileModel current = stack.pop();
 
-        if (current.equals(end)) {
-            current.status = TileStatus.PATH;
-            return true;
-        }
+if (current.equals(mazeController.getEndTile())) {
+isFinished = true;
+pathStep = current;
+return false;
+}
 
-        for (TileModel voisin : mazeController.maze.getAccessibleNeighbors(current)) {
-            if (explore(voisin, end)) {
-                current.status = TileStatus.PATH;
-                return true;
-            }
-        }
+for (TileModel neighbor : mazeController.maze.getAccessibleNeighbors(current)) {
+if (!visited.contains(neighbor)) {
+visited.add(neighbor);
+stack.push(neighbor);
+parentMap.put(neighbor, current);
+neighbor.status = TileStatus.VISITED;
+}
+}
 
-        return false;
-    }
+return false;
+}
+
+// No path found
+isFinished = true;
+return true;
+}
+
+return true;
+}
+
+@Override
+public int getVisitedCount() {
+return visited.size();
+}
+
+@Override
+public int getPathCount() {
+return pathCount;
+}
+
+@Override
+public boolean isComplete() {
+return isFinished && pathStep == null;
+}
 }
