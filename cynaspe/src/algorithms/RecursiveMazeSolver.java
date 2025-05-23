d93872d06f1b1e24e48da98673361378950fff98
WirbelWind03 <hidden>
2025-05-23 05:46:07 +0200

    Added comments

--- cynaspe/src/algorithms/RecursiveMazeSolver.java
+++ cynaspe/src/algorithms/RecursiveMazeSolver.java
@@ -20,40 +20,55 @@ public class RecursiveMazeSolver extends Solver implements ISolverAlgorithm {
 
     @Override
     public boolean step() {
+        // If the algorithm has finished, start tracing the path starting from the end tile
         if (isFinished && pathStep != null) {
+            // Mark the current tile as being the path
             pathStep.status = TileStatus.PATH;
             pathCount++;
+            // Move to the parent tile (previous step) in the path
             pathStep = parentMap.get(pathStep);
+            // True if path tracing has finished (no more parents)
             return pathStep == null;
         }
 
         if (!isFinished) {
+            // If there are tiles left to go
             if (!stack.isEmpty()) {
+                // Pop the next tile to go
                 TileModel current = stack.pop();
 
+                // If the current tile is the end tile
+                // The algoritm has finished
                 if (current.equals(mazeController.getEndTile())) {
                     isFinished = true;
                     pathStep = current;
+                    // return false because we have to do the path tracing
                     return false;
                 }
 
+                // Loop over the accessible neigbhors of the tile
                 for (TileModel neighbor : mazeController.maze.getAccessibleNeighbors(current)) {
+                    // If the neighbor wasn't been visited
                     if (!visited.contains(neighbor)) {
+                        // Add that the neighbor has been visited
                         visited.add(neighbor);
-                        stack.push(neighbor);
-                        parentMap.put(neighbor, current);
                         neighbor.status = TileStatus.VISITED;
+                        // Add the neigbhor to be explored later
+                        stack.push(neighbor);
+                        // Track that we went to this neighbor with the current tile
+                        parentMap.put(neighbor, current);
                     }
                 }
 
+                // Algorithm is still going
                 return false;
             }
 
-            // No path found
+            // No path has been found
             isFinished = true;
             return true;
         }
-
+        // End the algorithm
         return true;
     }
 
