28df55cc60b9893b9cbc2ab911c84741ae880fb8
WirbelWind03 <hidden>
2025-05-23 16:09:32 +0200

    Created MazeCanvasController

--- cynaspe/src/controller/MainController.java
+++ cynaspe/src/controller/MainController.java
@@ -38,12 +38,12 @@ import utils.SpinnerText;
 
 public class MainController extends Controller {
     private MazeController mazeController;
+    private MazeCanvasController mazeCanvasController;
+
     private ISolverAlgorithm solverAlgorithm = null;
 
     private GenerationMode selectedSolverMode = null;
     private SolveAlgorithms selectedSolverAlgorithms = null;
-
-    private AnimationTimer solverTimer = null;
 
     @FXML private Spinner<Integer> SpinnerGenerationSpeed; // Spinner that change the speed of the generation or the solve
     @FXML private Label LabelGenerationStatus; // Label that show the generation or solve status
@@ -66,7 +66,6 @@ public class MainController extends Controller {
     @FXML private Label LabelVisitedTiles; // Label showing the number of tiles visited
     @FXML private Label LabelGenerationTime; // Label showing the maze solving generation time
 
-    @FXML private MenuItem MenuItemMazeNew;
     @FXML private MenuItem MenuItemMazeLoad;
     @FXML private MenuItem MenuItemMazeSave;
 
@@ -74,14 +73,13 @@ public class MainController extends Controller {
     public void initialize(){
         // Set the value of the spinner for the generation speed
         SpinnerValueFactory<Integer> generationSpeedValueFactory = 
-            new SpinnerValueFactory.IntegerSpinnerValueFactory(1,120);
+            new SpinnerValueFactory.IntegerSpinnerValueFactory(1,60);
         generationSpeedValueFactory.setValue(10);
         SpinnerGenerationSpeed.setValueFactory(generationSpeedValueFactory);
 
         // Create the maze controller for the mazeCanvas
         mazeController = new MazeController(mazeCanvas);
-        mazeCanvas.setFocusTraversable(true);
-        mazeCanvas.setOnKeyPressed(this::onMazeCanvasKeyPressed);
+        mazeCanvasController = new MazeCanvasController(mazeCanvas, mazeController);
 
         // Initialize
         RadioButtonMazeSolverDFS.setUserData(SolveAlgorithms.DFS);
@@ -104,11 +102,6 @@ public class MainController extends Controller {
         });
     }
 
-    /**
-     * 
-     * @param event
-     * @throws Exception
-     */
     @FXML
     private void MBMazeNew_Click(ActionEvent event) throws Exception{
         // Open the MazeConfigurationView
@@ -129,119 +122,6 @@ public class MainController extends Controller {
             constructMaze(mazeConfigurationController);
         }
     }
-
-    //#region MazeCanvas events
-
-    /**
-     * When the user mouse hover over the MazeCanvas
-     * @param event
-     * The mouse hovering over
-     */
-    @FXML
-    private void onMazeCanvasEntered(MouseEvent event){
-        mazeCanvas.requestFocus();
-    }
-
-    /**
-     * When the user mouse click on the MazeCanvas
-     * @param event
-     * The mouse that has clicked
-     */
-    @FXML
-    private void onMazeCanvasClicked(MouseEvent event) {
-        if (mazeController.isGenerating || !mazeController.hasMaze() || mazeController.hoveredTile == null) return;
-        
-        // Add a wall with the direction the user want
-        mazeController.addWall(mazeController.hoveredTile, mazeController.hoveredWall);
-
-        // Render the maze with the new added wall
-        mazeController.renderMaze(false);
-    }
-
-    /**
-     * When the user mouse move on the canvas
-     * @param event
-     * The mouse that has entered the canvas
-     */
-    @FXML
-    private void onMazeCanvasMouseMoved(MouseEvent event) {
-        if (mazeController.isGenerating || !mazeController.hasMaze()) return;
-    
-        // Get the position of the mouse
-        double x = event.getX();
-        double y = event.getY();
-    
-        // Calculate the tileSize
-        double tileSize = mazeController.getTileSize();
-    
-        // Get the row and column the mouse is in
-        int column = (int)(x / tileSize);
-        int row = (int)(y / tileSize);
-    
-        // Assign the hovered tile by getting the tile where the mouse is
-        if (mazeController.isInsideMaze(row, column)) {
-            mazeController.hoveredTile = mazeController.getTile(row, column); 
-        }
-    }
-
-    /**
-     * When the user mouse exit the canvas
-     * @param mouseEvent
-     * The mouse that has exited
-     */
-    @FXML
-    private void onMazeCanvasMouseExited(MouseEvent mouseEvent){
-        mazeController.hoveredTile = null;
-    }
-
-    /**
-     * When the user press a key on the canvas
-     * @param event
-     * The key that has been pressed
-     */
-    @FXML
-    private void onMazeCanvasKeyPressed(KeyEvent event){
-        if (mazeController.isGenerating || !mazeController.hasMaze() || mazeController.hoveredTile == null) return;
-
-        switch (event.getCode()) {
-            // ZQSD keys
-            case Z:
-                // If the user is at the top border
-                if (mazeController.hoveredTile.row == 0)
-                    return;
-                mazeController.hoveredWall = WallDirection.TOP;
-                break;
-            case D:
-                // If the user is at the right border
-                if (mazeController.hoveredTile.column == mazeController.maze.numCols - 1)
-                    return;
-                mazeController.hoveredWall = WallDirection.RIGHT;
-                break;
-            case S:
-                // If the user is at the bottom border
-                if (mazeController.hoveredTile.row == mazeController.maze.numRows - 1)
-                    return;
-                mazeController.hoveredWall = WallDirection.BOTTOM;
-                break;
-            case Q:
-                // If the user is at the left border
-                if (mazeController.hoveredTile.column == 0)
-                    return;
-                mazeController.hoveredWall = WallDirection.LEFT;
-                break;
-            // Del key
-            case DELETE:
-                mazeController.removeWall(mazeController.hoveredTile, mazeController.hoveredWall);
-                break;
-            default:
-                return;
-        }
-
-        // Render the wall that is going to be added
-        mazeController.renderMaze(false);
-    }
-
-    //#endregion
 
     //#region MenuItem
 
@@ -355,7 +235,7 @@ public class MainController extends Controller {
                 LabelGenerationStatus.setText(spinner.getCurrentFrame());
                 
                 // Start the animation
-                solverTimer = new AnimationTimer() {
+                AnimationTimer timer = new AnimationTimer() {
                     private long lastUpdate = 0;
 
                     @Override
@@ -390,7 +270,7 @@ public class MainController extends Controller {
                     }
                 };
 
-                solverTimer.start();
+                timer.start();
                 break;
         
             
@@ -406,15 +286,7 @@ public class MainController extends Controller {
         mazeController.isGenerating = true;
         
         mazeController.resetTileStatus();
-        // Disable the button until the solver has finished
         MazeButtonSolve.setDisable(true);
-
-        // Desactivate these menu items
-        MenuItemMazeNew.setDisable(true);
-        // The user shouldn't be able to load a maze during the generation
-        MenuItemMazeLoad.setDisable(true);
-        // The user shouldn't be able to save a unfinished maze
-        MenuItemMazeSave.setDisable(true);
 
         switch (selectedSolverAlgorithms) {
             case SolveAlgorithms.DFS:
@@ -454,13 +326,12 @@ public class MainController extends Controller {
                         public void handle(long now) {
                             if (now - lastUpdate >= Helpers.fpsToNanos(SpinnerGenerationSpeed.getValue())) {
                                 lastUpdate = now;
-                                // Jump to the next step of the algorithm
+    
                                 boolean done = solverAlgorithm.step();
                                 mazeController.renderMaze(false);
                                 updateSolverLabels();
-                                // Show the next frame of the spinner
-                                LabelGenerationStatus.setText(spinner.nextFrame());
-                                // If the algorithm has finished
+                                spinner.nextFrame();
+                                LabelGenerationStatus.setText(spinner.getCurrentFrame());
                                 if (done){
                                     stop();
                                     finishedSolving();
@@ -478,20 +349,13 @@ public class MainController extends Controller {
         
     }
 
-    /**
-     * Update when the solver has finished
-     */
     private void finishedSolving(){
         mazeController.isGenerating = false;
         updateSolverLabels();
         LabelGenerationStatus.setText("Traitement terminée");
-        MenuItemMazeNew.setDisable(false);
         MazeButtonSolve.setDisable(false);
     }
 
-    /**
-     * Update label that show information of the solve
-     */
     private void updateSolverLabels(){
         LabelVisitedTiles.setText(String.format("Traitées : %d", solverAlgorithm.getVisitedCount()));
         LabelPath.setText(String.format("Chemin final : %d", solverAlgorithm.getPathCount()));
